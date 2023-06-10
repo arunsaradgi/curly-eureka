729d07150f58156882a29cb9eb5acb57abd0dff0
Arun Saradgi <saradgi07@gmail.com>
2023-06-10 17:43:15 +0530

    bcc

--- db.js
+++ db.js
@@ -1,8 +1,18 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const connection = mongoose.connect(process.env.mongoURL);
+mongoose.set("strictQuery", false);
+
+const connectDB = async () => {
+  try {
+    const connection = await mongoose.connect(process.env.mongoURL);
+    console.log("connected to mongoDB");
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
 
 module.exports = {
-  connection,
+  connectDB,
 };
